cadcb5090a9bae01026a09488a2daaff001e23a0
Matt H <mhh129@gmail.com>
2021-03-20 00:26:25 -0400

    Add password mismatch validation to 'Create an Account' section of login component

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { 
+  AbstractControl, 
+  FormBuilder, 
+  FormControl,
+  FormGroup, 
+  Validators, 
+  ValidationErrors, 
+  ValidatorFn 
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,28 +15,40 @@ import { FormBuilder } from '@angular/forms';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  createForm = this.fb.group({
-    email: '',
-    password: '',
-    passwordAgain: '',
-  })
-
-  loginForm = this.fb.group({
-    email: '',
-    password: '',
-  })
+  createForm: FormGroup = this.fb.group({});
+  loginForm: FormGroup = this.fb.group({});
 
   constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.createForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      passwordAgain: ['', [Validators.required, this.checkPasswordsMatch(this.createForm)]],
+    });
+  
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required]],
+      password: ['', [Validators.required]],
+    })
+  }
+
+  checkPasswordsMatch(formGroup: FormGroup): ValidatorFn {
+    return (formControl: any): any => {
+
+      console.log('--- formGroup:', formGroup)
+      console.log('--- formControl:', formControl)
+      
+      const match = this.createForm?.get('password')?.value === this.createForm?.get('passwordAgain')?.value;
+      return match ? null : { passwordMismatch: true };
+    }
   }
 
   onSubmitLogin(): void {
-    console.log('--- loginForm:', this.loginForm.value)
+    console.log('--- loginForm:', this.loginForm)
   }
 
   onSubmitCreate(): void {
-    console.log('--- createForm:', this.createForm.value)
+    console.log('--- createForm:', this.createForm)
   }
-
 }
